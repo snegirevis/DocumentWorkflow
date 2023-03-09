2e0aef447d198c65bb0687c49230e0c717368c7e
snegirevis <sngraindrops@gmail.com>
2023-03-10 01:36:16 +0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+
   <router-view/>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  height: 100vh;
+  width: 100vw;
+  background: #ECEBF3;
 }
 
-nav {
-  padding: 30px;
-}
+*{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+    
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/StartView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="start_view">
+    <h1>Добро пожаловать!</h1>
+  </div>
+</template>
+
+<script>
+
+
+</script>
+
+<style scoped>
+h1{
+    font-family: 'Montserrat', sans-serif;
+}
+</style>
